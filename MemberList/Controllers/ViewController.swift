ab1aa2b40aca247b1250a77b4e49992ceb34b0f7
yw22 <youngwook.dev@gmail.com>
2022-06-03 19:46:30 +0900

    MemberList Init

---
+++ MemberList/Controllers/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by 김영욱 on 2022/06/03.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // 테이블 뷰
+    private let tableView = UITableView()
+    
+    var memberListManager = MemberListManage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        
+        setupTableViewConstraints()
+    }
+    
+    // 테이블뷰의 오토레이아웃 설정
+    func setupTableViewConstraints() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+    
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
---
+++ MemberList/Models/Member.swift
@@ -0,0 +1,48 @@
+//
+//  Member.swift
+//  MemberList
+//
+//  Created by 김영욱 on 2022/06/03.
+//
+
+import Foundation
+import UIKit
+
+struct Member {
+    
+    lazy var memberImage: UIImage? = {
+        // 이름이 없다면, 시스템 사람이미지 셋팅
+        guard let name = name else {
+            return UIImage(systemName: "person")
+        }
+        
+        // 해당이름으로된 이미지가 없다면, 시스템 사람이미지 셋팅
+        return UIImage(named: "\(name).png") ?? UIImage(systemName: "person")
+        
+    }()
+    
+    // 멤버의 절대적 순서를 위한 타입 저장 속성
+    static var memberNumbers: Int = 0
+    
+    let memberId: Int
+    var name: String?
+    var age: Int?
+    var phone: String?
+    var address: String?
+    
+    init(name: String?, age: Int?, phone: String?, address: String?) {
+        
+        // 0 일떄는 0, 0이 아닐때는 타입저장속성의 절대적 값을 셋팅
+        self.memberId = Member.memberNumbers
+        
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.address = address
+        
+        // 멤버를 생성한다면, 항상 타입 저장속성의 값을 +1 을 한다
+        Member.memberNumbers += 1
+        
+    }
+    
+}
